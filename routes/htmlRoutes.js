96c767c3a6a626d1ed5789c14786f54a2b3026e9
Gian Zamora <gz2016@att.net>
2021-06-04 01:06:20 -0700

    added html load routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const path = require('path');
+
+// sets up index.html (mainpage) route
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// sets up exercise.html (individual exercise) route
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
+
+// sets up stats.html route
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
